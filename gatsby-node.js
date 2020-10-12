6a38617593d55b3b39fbc6460207ce680744da21
Matthew Jurenka <matt.jurenka@comcast.net>
2020-10-12 02:03:46 +0100

    removed blob fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,15 @@
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     actions.setWebpackConfig({
       node: {
         fs: 'empty'
-      }
+      },
+      module: stage === "build-html" ? {
+        rules: [
+          {
+            test: /web-audio-beat-detector/,
+            use: loaders.null()
+          }
+        ]
+      } : undefined
     })
   }
--- src/settings.ts
+++
@@ -1,2 +0,0 @@
-const Blob = require("cross-blob");
-globalThis.Blob = Blob;
