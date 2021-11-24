7af6f764a2beaa96cd6f80784648f01693ea7e82
Matthew Jurenka <matt.jurenka@comcast.net>
2021-11-24 09:24:59 -0800

    Added sitemap and updated gatsby version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,55 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+  * Configure your Gatsby site with this file.
+  *
+  * See: https://www.gatsbyjs.com/docs/gatsby-config/
+  */
+
+const siteUrl = process.env.URL || "https://www.matthewjurenka.com"
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          "Josefin Sans",
-          "Crimson Text"
-        ],
-      },
-    }, `gatsby-plugin-react-helmet`
-  ],
+    siteMetadata: {
+        siteUrl: "https://www.matthewjurenka.com",
+    },
+    /* Your site config here */
+    plugins: [
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [
+                    "Josefin Sans",
+                    "Crimson Text"
+                ],
+            },
+        }, `gatsby-plugin-react-helmet`,
+        {
+            resolve: "gatsby-plugin-sitemap",
+            options: {
+                query: `
+                    {
+                        allSitePage(filter: {
+                            path: {glob: "!/visualizations/*_renderer/"}
+                        }) {
+                            nodes {
+                                path
+                            }
+                        }
+                    }
+                `,
+                resolveSiteUrl: () => siteUrl,
+                resolvePages: ({
+                    allSitePage: { nodes: allPages },
+                }) => {
+                    return allPages.map(page => {
+                        return { ...page }
+                    })
+                },
+                serialize: ({ path, modifiedGmt }) => {
+                    return {
+                        url: path,
+                        lastmod: modifiedGmt,
+                    }
+                },
+            },
+        }
+    ],
 }
