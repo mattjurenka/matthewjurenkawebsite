86d921b8f9bf0dfc471878ee61cbb07e1dd9c163
Matthew Jurenka <matt.jurenka@comcast.net>
2022-03-17 08:55:55 -0700

    fixed unresponsive front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Grid, Typography, useMediaQuery } from "@mui/material"
+import { Box, Grid, Typography, useMediaQuery } from "@mui/material"
 import Experiences from "../sections/Experiences"
 import Metadata from "../components/metadata"
 
@@ -10,59 +10,72 @@ import Projects from "../sections/Projects"
 
 
 export default () => {
-    const is_md = useMediaQuery(theme.breakpoints.up("sm"))
-    const is_lg = useMediaQuery(theme.breakpoints.up("md"))
+    const is_sm = useMediaQuery(theme.breakpoints.up("sm"))
+    const is_md = useMediaQuery(theme.breakpoints.up("md"))
+    const is_lg = useMediaQuery(theme.breakpoints.up("lg"))
 
     return <Metadata
         title={"Home | Matthew Jurenka"}
         description={intro}
     >
-        <Grid
-            container
-            style={{
-                width: "100%",
-                margin: "0 auto",
-                marginBottom: "8rem",
-                paddingTop: is_md ? "16vh" : "8vh",
-                paddingLeft: is_md ? "20%" : 0,
-                paddingRight: is_md ? "20%" : 0,
-                overflow: "hidden",
-            }}
-            spacing={8}
-        >
-            <Grid item xs={12}>
-                <Header />
+        <Box sx={{
+            flexGrow: 1,
+            margin: "0 auto",
+            marginBottom: "8rem",
+            paddingTop: is_sm ? "16vh" : "8vh",
+            paddingLeft: is_lg ? "20%" : is_sm ? "10%" : "2rem",
+            paddingRight: is_lg ? "20%" : is_sm ? "10%" : "2rem",
+            overflow: "hidden",
+        }}>
+            <Grid
+                container
+                //style={{
+                //    width: "100%",
+                //    margin: "0 auto",
+                //    marginBottom: "8rem",
+                //    paddingTop: is_sm ? "16vh" : "8vh",
+                //    paddingLeft: is_md ? "20%" : 0,
+                //    paddingRight: is_md ? "20%" : 0,
+                //    overflow: "hidden",
+                //}}
+                spacing={8}
+            >
+                <Grid item xs={12}>
+                    <Header />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="body1">
+                        {intro}
+                    </Typography>
+                    <Typography variant="body1" style={{marginTop: "1em"}}>
+                        {intent}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Experiences />
+                </Grid>
+                <Grid item xs={6} style={{position: "relative", zIndex: -1}}> 
+                    <img src={"/mountains.png"} style={{
+                        position: "absolute",
+                        zIndex: -1,
+                        transform: "translate(-45%, -42.5%) scale(.5)",
+                    }}/>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Projects
+                        title="Featured Projects"
+                        projects={featured_projects}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6} style={{
+                    marginTop: is_md ? "-32em" : "initial"
+                }}>
+                    <Projects
+                        title="More Projects"
+                        projects={more_projects}
+                    />
+                </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-                <Typography variant="body1">
-                    {intro}
-                </Typography>
-                <Typography variant="body1" style={{marginTop: "1em"}}>
-                    {intent}
-                </Typography>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Experiences />
-            </Grid>
-            <Grid item xs={6} style={{position: "relative", zIndex: -1}}> 
-                <img src={"/mountains.png"} style={{
-                    position: "absolute",
-                    zIndex: -1,
-                    transform: "translate(-45%, -42.5%) scale(.5)",
-                }}/>
-            </Grid>
-            <Grid item xs={12} md={6}>
-                <Projects
-                    title="Featured Projects"
-                    projects={featured_projects}
-                />
-            </Grid>
-            <Grid item xs={12} md={6} style={{marginTop: is_lg ? "-32em" : "initial"}}>
-                <Projects
-                    title="More Projects"
-                    projects={more_projects}
-                />
-            </Grid>
-        </Grid>
+        </Box>
     </Metadata>
 }
